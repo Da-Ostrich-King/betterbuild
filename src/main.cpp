33e2839c80eed35e46be7441c97a3a8a4fa4ec43
Liam Waaga <liamwaaga@gmail.com>
2024-12-02 11:40:53 -0800

    write help text

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,17 @@
 
 #include <iostream>
 #include <unistd.h>
+char const * helpText = 
+    "Name\n"
+    "   BetterBuild\n"
+    "Usage\n"
+    "   bbuild {configname | \"default\"}\n"
+    "Options\n"
+    "   -h, --help  Display this help text\n"
+    "Examples\n"
+    "   bbuild /* runs the \"default\" config, commonly builds with default options */"
+    "   bbuild install /* runs the \"install\" config, commonly used to install built executables to the system */"
+    "   bbuild menuconfig /* runs the \"menuconfig\" config, commonly used to do build options*/";
 
 
 int main( int argc, char **argv, char **envv ) {
@@ -10,14 +21,14 @@ int main( int argc, char **argv, char **envv ) {
     char doBuild;
     if ( !args.noCheck ) {
         std::cout << "This can run arbitrary commands, read the BuildFile and ensure it isn't doing anything malicous. Are you sure you want to continue? (Y, n): ";
-        std::cin.get(doBuild);        
+        std::cin.get( doBuild );        
     } else {
         doBuild = 'y'; // assume yes if noCheck is true
     }
     
     if ( doBuild == '\n' || doBuild == 'y' ) {
         if ( args.config.find( std::string( "install" ) ) == std::string::npos ) {
-            if ( !geteuid() ) {
+            if ( geteuid() != 0 ) {
                 std::cout << "Must not be ran as root unless running a config with the substring \"install\"\n";
                 exit( 1 );
             }
